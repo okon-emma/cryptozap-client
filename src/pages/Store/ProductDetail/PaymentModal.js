7468d3c877285665049130c43601fc022941db6e
Okon Emmanuel <drefiom@gmail.com>
2021-10-17 12:25:37 +0100

    Payment Modal

--- src/pages/Store/ProductDetail/PaymentModal.js
+++ src/pages/Store/ProductDetail/PaymentModal.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./paymentModal.css";
-import { Form } from "react-bootstrap";
 
 function PaymentModal({ show, handleClose }) {
   const showHideClassName = show ? "modal display-block" : "modal display-none";
